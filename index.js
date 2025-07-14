c6f2c34b7414a858f22c217bff9b7c8bab9893b5
Nishavaghela3 <vaghelanisha393@gmail.com>
2025-07-14 11:23:02 +0530

    name

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const name="NISHA";
 console.log(name,"Vaghela");
 
 
+// odd even
+
+
+const a=7
+if (a%2==0)
+{
+    console.log("The number is EVEN");
+}
+else
+{
+    console.log ("THe number is ODD");
+}
