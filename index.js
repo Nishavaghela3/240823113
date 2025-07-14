e3a25285811c44b20eebf5995e711cff917303f5
Nishavaghela3 <vaghelanisha393@gmail.com>
2025-07-14 11:25:55 +0530

    name

--- index.js
+++ index.js
@@ -15,3 +15,14 @@ else
 {
     console.log ("THe number is ODD");
 }
+
+// MAx from 2
+const b=10
+if (a>b)
+{
+    console.log("The max Number is : ",a);
+}
+else
+{
+    console.log ("The max Number is : ",b);
+}
