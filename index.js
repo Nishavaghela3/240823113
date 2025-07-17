8dc5172dda8d578845c05da7f0f659ded480fca1
nisha393 <vaghelanisha393@gmail.com>
2025-07-17 08:38:49 +0530

    name

--- index.js
+++ index.js
@@ -55,12 +55,46 @@
 
 // factorial of a number  5
 
-const f=5;
-let fact=1;
+// const f=5;
+// let fact=1;
 
-for(let i=1;i<=f;i++)
+// for(let i=1;i<=f;i++)
 
-{
-    fact*=i;
+// {
+//     fact*=i;
+// }
+// console.log("The factorial of ",f," is: ",fact);
+
+//table 6
+
+// const t=3;
+// for(let i=1;i<=10;i++)
+// {
+//     console.log(t,"*",i,"=",t*i);        
+// }
+
+// Reverse of a number
+
+// for(let i=4;i>=1;i--)
+// { 
+//     console.log(i);
+// }
+
+
+//palindrome 
+let num = 121;
+let original = num;
+let reversed = 0;
+
+while (num > 0) {
+    let digit = num % 10;
+    reversed = reversed * 10 + digit;
+    num = Math.floor(num / 10);
 }
-console.log("The factorial of ",f," is: ",fact);
+
+if (original === reversed) {
+    console.log("It is a palindrome.");
+} else {
+    console.log("It is not a palindrome.");
+}
+
