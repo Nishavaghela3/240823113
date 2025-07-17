80f373ebf88e27d5d1d5a67fc443e9fae5c9610b
nisha393 <vaghelanisha393@gmail.com>
2025-07-17 08:07:45 +0530

    name

--- index.js
+++ index.js
@@ -1,54 +1,66 @@
-console.log(" HELLO ");
-const name="NISHA";
-console.log(name,"Vaghela");
+// console.log(" HELLO ");
+// const name="NISHA";
+// console.log(name,"Vaghela");
 
 
 // odd even
 
 
-const a=7
-if (a%2==0)
-{
-    console.log("The number is EVEN");
-}
-else
-{
-    console.log ("THe number is ODD");
-}
+// const a=7
+// if (a%2==0)
+// {
+//     console.log("The number is EVEN");
+// }
+// else
+// {
+//     console.log ("THe number is ODD");
+// }
 
 // MAx from 2
-const b=10
-if (a>b)
-{
-    console.log("The max Number is : ",a);
-}
-else
-{
-    console.log ("The max Number is : ",b);
-}
+// const b=10
+// if (a>b)
+// {
+//     console.log("The max Number is : ",a);
+// }
+// else
+// {
+//     console.log ("The max Number is : ",b);
+// }
 
 
 // Leap year or not
 
-const y=2024
+// const y=2024
 
-if((y % 4 === 0 && y%100!==0)||(y%400===0))
-{
-    console.log("the year: ",y,"Is a Leap Year");
-}
-else{
-    console.log("the year: ",y,"Is not a Leap Year")
-}
+// if((y % 4 === 0 && y%100!==0)||(y%400===0))
+// {
+//     console.log("the year: ",y,"Is a Leap Year");
+// }
+// else{
+//     console.log("the year: ",y,"Is not a Leap Year")
+// }
 
 
 // sum of n numbers
 
-const n=10;
-const s=0;
-for(let i=1;i<=n;i++)
-{
+// const n=10;
+// const s=0;
+// for(let i=1;i<=n;i++)
+// {
 
-    s+=i;
+//     s+=i;
    
+// }
+// console.log(s);
+
+// factorial of a number  5
+
+const f=5;
+let fact=1;
+
+for(let i=1;i<=f;i++)
+
+{
+    fact*=i;
 }
-console.log(s);
+console.log("The factorial of ",f," is: ",fact);
